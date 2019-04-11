4670453a1ab8ed46be7cb2736f938a83a05de650
Blackout <36774775+Codebreak1@users.noreply.github.com>
2019-04-11 01:16:33 -0400

    Update banners.py

--- config/banners.py
+++ config/banners.py
@@ -62,4 +62,4 @@ def print_banner():
 <<--[[ """+Fore.MAGENTA+"""NetHack Alpha 1.0.0"""+Fore.WHITE+""" ]}
     
         __-[0 exploits - 0 addons  - 0 frameworks]-__
-        __-[2 develops - 3 modules - 4 banners   ]-__\n""")
+        __-[2 develops - 3 modules - 4 banners   ]-__ by Codebreak1\n""")
