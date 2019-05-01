7105435afa5e8c5ba4ca5bc3aac6d3625f9f2e9d
Codebreak1 <nicolas.schnitz@gmail.com>
2019-05-01 02:23:37 -0400

    Archivos modificados, fix y mejor codigo

--- config/colors.py
+++ config/colors.py
@@ -1,7 +1,9 @@
-red = "\033[0;31m"
-blue = "\033[0;34m"
-green = "\033[0;32m"
-yellow = "\033[0;33m"
-white = "\033[0;37m"
-violet = "\033[0;35m"
-cian = "\033[0;36m"
+
+def colors():
+    red = "\033[0;31m"
+    blue = "\033[0;34m"
+    green = "\033[0;32m"
+    yellow = "\033[0;33m"
+    white = "\033[0;37m"
+    violet = "\033[0;35m"
+    cian = "\033[0;36m"
