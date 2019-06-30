63678bd7e0be2e768b8b04c93fb575077c9caeb6
Blackout <36774775+Codebreak1@users.noreply.github.com>
2019-06-30 00:46:18 -0400

    Yup
    Nothing important

---
+++ logo.py
@@ -0,0 +1,16 @@
+from colorama import *
+# - Team Hackers Sudametrica's logo - #
+
+print(Fore.LIGHTBLACK_EX+"[THS._] [0.0.1] [] [] [] [] [] [] [] [] [] [] [] [] [] [] []\n")
+print(Fore.GREEN+"> hack\t\t\t\t\t\t\t  "+Fore.LIGHTBLACK_EX+"[]")
+print(Fore.LIGHTBLACK_EX+"[*] Downloading website . . .\t[Sesion1]")
+print("     #Installing backdoor\t[Sesion1-TheFatRat]\t  []")
+print("     #Connecting to TheFatRat\t[Sesion1-TheFatRat]")
+print(Fore.LIGHTGREEN_EX+"//Succesfuly \t[Sesion1]\t\t\t\t  "+Fore.LIGHTBLACK_EX+"[]\n"+Fore.GREEN)
+print(Fore.LIGHTBLACK_EX+" [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] \n")
+print(Fore.WHITE+"""\
+<<--[[ """+Fore.MAGENTA+"""Sudametrica Alpha 1.0.0"""+Fore.WHITE+""" ]}
+
+        __-[0 exploits - 0 addons  - 0 frameworks]-__
+        __-[2 develops - 3 modules - 4 banners   ]-__ by Catsploit\n""")
+print("\033[4;31m" + "admin" + "\033[0;31m" + " >{} join us!".format(Fore.WHITE))
